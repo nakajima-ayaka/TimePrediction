cdc82bc2adac5c3ad6e34e6dd0ee63f31ff95044
id <id@IN0013474>
2021-07-07 16:16:09 +0900

    【開発演習】
    2021/07/07
    
    修正内容：
    user.javaの追加
    UserRepository.javaの追加
    AccountController.javaの追加
    ログイン処理の追加

---
+++ src/main/java/com/example/demo/AccountController.java
@@ -0,0 +1,113 @@
+package com.example.demo;
+
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AccountController {
+
+	//他クラスとの繋がりを宣言する記述が大幅に減るアノテーション
+	@Autowired
+	//セッションスコープを使えるようにする
+	HttpSession session;
+
+	@Autowired
+	//UserRepositoryを使えるようにする
+	private UserRepository userRepository;
+
+	@RequestMapping("/")
+	public String login() {
+
+		// セッション情報はクリアする
+		session.invalidate();
+
+		//ログインへ遷移
+		return "login";
+	}
+
+	//ログイン(入力済み)
+	@PostMapping("/login")
+	public ModelAndView login(
+			@RequestParam("email") String email,
+			@RequestParam("password") String password,
+			ModelAndView mv) {
+
+		//メールアドレスorパスワードが空の場合にエラーとする
+		if (email == null || email.length() == 0 || password == null || password.length() == 0) {
+
+			//表示のオブジェクト
+			mv.addObject("message", "入力してください");
+
+			//ログインへ遷移を指定
+			mv.setViewName("login");
+
+			return mv;
+		}
+
+		//メールアドレス検索結果からパスワードを取得
+		findEmail(email);
+
+		String pass = findEmail(email);
+
+			//パスワードが一致するか確認
+			if (pass == password) {
+
+				// セッションスコープにuser情報を格納する
+				session.setAttribute("user", userRepository.findAll());
+
+				//天候選択へ遷移を指定
+				mv.setViewName("weather");
+
+				return mv;
+
+			} else {
+				//不一致verのオブジェクトの生成
+				mv.addObject("message", "メールアドレスもしくはパスワードが間違っています");
+
+				//ログインへ遷移を指定
+				mv.setViewName("login");
+
+				return mv;
+			}
+		}
+
+	//メールアドレス検索
+	public String findEmail(String email) {
+
+		//Userの宣言
+		User user;
+
+		//メールアドレスでの検索結果を取得
+		Optional<User> record = userRepository.findByEmail(email);
+
+		//recordの有無の判定
+		if (!record.isEmpty()) {
+			user = record.get();
+		} else {
+			return null;
+		}
+
+		//情報取得
+		String password = user.getPassword();
+
+		//顧客情報をセッションスコープに設定
+		//session.setAttribute("customerInfo", user);
+
+		return password;
+	}
+
+	//		@RequestParam("leaveHomeTime") int leaveHomeTime,
+	//		@RequestParam("commuterCode1") int commuterCode1,
+	//		@RequestParam("commuterCode2") int commuterCode2,
+	//		@RequestParam("commuterCode3") int commuterCode3,
+	//		@RequestParam("stationCompanyTime") int stationCompanyTime,
+
+}
---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,136 @@
+package com.example.demo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//このクラスがJPAのエンティティ・クラスである意味
+@Entity
+//エンティティに対応するテーブル名を指定※クラス名がテーブル名と同じでない時に使用
+@Table(name = "users")
+public class User {
+
+	//フィールド
+	@Id //主キーの指定
+	//↓シリアルで設定されているコードについてつける＠
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int code; //会員番号
+	private String email; //メールアドレス
+	private String password; //パスワード
+
+	@Column(name="leave_home_time")
+	private int leaveHomeTime; //いつも家を出る時間
+
+	@Column(name="home_station_time")
+	private int homeStationTime; //自宅から最寄り駅までの所要時間(分)
+
+	@Column(name="commuter_code1")
+	private int commuterCode1; //通勤経路１つめ
+
+	@Column(name="commuter_code2")
+	private int commuterCode2; //通勤経路２つめ
+
+	@Column(name="commuter_code3")
+	private int commuterCode3; //通勤経路３つめ
+
+	@Column(name="station_company_time")
+	private int stationCompanyTime; //駅から会社までの所要時間(分)
+
+	//コンストラクタ(引数なし)
+	public User() {
+
+	}
+
+	//コンストラクタ(全部入り)
+	public User(
+			int code, String email, String password, int leaveHomeTime,
+			int commuterCode1, int commuterCode2, int commuterCode3, int stationCompanyTime
+			) {
+
+	}
+
+	//登録の時はこのコンストラクタ(DBがシリアルだから)
+	public User(
+			String email, String password, int leaveHomeTime,
+			int commuterCode1, int commuterCode2, int commuterCode3, int stationCompanyTime
+			) {
+
+	}
+
+	//アクセッサ・メソッド(セッタ＆ゲッタ)
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getLeaveHomeTime() {
+		return leaveHomeTime;
+	}
+
+	public void setLeaveHomeTime(int leaveHomeTime) {
+		this.leaveHomeTime = leaveHomeTime;
+	}
+
+	public int getHomeStationTime() {
+		return homeStationTime;
+	}
+
+	public void setHomeStationTime(int homeStationTime) {
+		this.homeStationTime = homeStationTime;
+	}
+
+	public int getCommuterCode1() {
+		return commuterCode1;
+	}
+
+	public void setCommuterCode1(int commuterCode1) {
+		this.commuterCode1 = commuterCode1;
+	}
+
+	public int getCommuterCode2() {
+		return commuterCode2;
+	}
+
+	public void setCommuterCode2(int commuterCode2) {
+		this.commuterCode2 = commuterCode2;
+	}
+
+	public int getCommuterCode3() {
+		return commuterCode3;
+	}
+
+	public void setCommuterCode3(int commuterCode3) {
+		this.commuterCode3 = commuterCode3;
+	}
+
+	public int getStationCompanyTime() {
+		return stationCompanyTime;
+	}
+
+	public void setStationCompanyTime(int stationCompanyTime) {
+		this.stationCompanyTime = stationCompanyTime;
+	}
+
+}
