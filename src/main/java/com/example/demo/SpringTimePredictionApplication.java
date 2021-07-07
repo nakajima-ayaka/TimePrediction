d7f8d4e5e00756507bb030e68dd4710dec2d0c9a
id <id@IN0013474>
2021-07-07 13:37:00 +0900

    【開発演習】 2021/07/07 内容：新規登録

---
+++ src/main/java/com/example/demo/SpringTimePredictionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTimePredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTimePredictionApplication.class, args);
+	}
+
+}
