51a661d2172484de1dac27f76ce1ee2bde683bc0
id <id@IN0013474>
2021-07-08 15:43:27 +0900

    【開発演習】
    2021/07/08
    
    修正内容：
    Railway.javaの修正(遅延頻度取得処理)の追加

--- src/main/java/com/example/demo/Railway.java
+++ src/main/java/com/example/demo/Railway.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.Optional;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,11 +9,17 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 //このクラスがJPAのエンティティ・クラスである意味
 @Entity
 //エンティティに対応するテーブル名を指定※クラス名がテーブル名と同じでない時に使用
 @Table(name = "railway")
 public class Railway {
+
+	@Autowired
+	//RailwayRepositoryを使えるようにする
+	private RailwayRepository railwayRepository;
 
 	//フィールド
 	@Id //主キーの指定
@@ -21,7 +29,6 @@ public class Railway {
 	private String name; //鉄道名
 	@Column(name = "delay_frequency")
 	private int delayFrequency; //遅延頻度
-
 
 	//コンストラクタ(引数なし)
 	public Railway() {
@@ -48,5 +55,35 @@ public class Railway {
 		return delayFrequency;
 	}
 
+	//遅延頻度取得処理
+	public String DelayFrequency(int code) {
+
+		//Railway宣言
+		Railway railway = null;
+
+		//鉄道番号での検索結果を取得
+		Optional<Railway> record = railwayRepository.findById(code);
+
+		//recordの有無の判定
+		if (record.isEmpty()) {
+			return null;
+		}
+
+		//一件のrailwayの情報取得
+		railway = record.get();
+
+		//遅延頻度の取得
+		int delay = railway.getDelayFrequency();
+
+		//3であればString型の鉄道名で返す、それ以外はnullで返す分岐
+		if (delay != 3) {
+			return null;
+
+		}
+		//3の場合の処理
+
+		return railway.getName();
+
+	}
 
 }
